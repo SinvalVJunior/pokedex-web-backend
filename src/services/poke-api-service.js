c3f03c0370b1861350a0bd92ca7bd93bbf64dce4
Sinval <sinvalvieirajunior@gmail.com>
2021-08-15 21:43:41 -0300

    Refactoring

--- src/services/poke-api-service.js
+++ src/services/poke-api-service.js
@@ -1,10 +1,8 @@
 const PokeAPIClient = require('../clients/poke-api/poke-api');
-const UserService = require('./user-service');
 class PokeAPIService {
 
     constructor() {
         this.PokeAPI = new PokeAPIClient();
-        this.userService = new UserService();
     }
 
     async getPokemons(numberOfPokemons) {
